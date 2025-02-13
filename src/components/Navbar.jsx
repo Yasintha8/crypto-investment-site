651947a4bb5fbf871077d8bf426ea4606ff4606f
Yasintha8 <yasinthachamikara28@gmail.com>
2025-02-14 01:22:02 +0530

    logo change2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,6 @@
 import { Menu, X} from "lucide-react";
 import { useState } from "react";
-
-import logo from "../assets/logo.png"
+import logo from "../assets/logo.png";
 import { navItems } from "../constants";
 const Navbar = () => {
     const [ mobileDrawerOpen, setmobileDrawerOpen ] = useState(false);
@@ -14,7 +13,7 @@ const Navbar = () => {
             <div className="container px-4 mx-auto relative lg:text-sm">
                 <div className="flex justify-between items-center">
                     <div className="flex items-center flex-shrink-0">
-                        <img className="w-10 h-10 mr-2" src={logo} alt="logo" />
+                        <img className="w-10 h-10 mr-2" src={logo} alt="Logo" />
                         <span className="text-xl tracking-tight"><a href="#Header">InvestMint</a></span>
                     </div>
                     <ul className="hidden lg:flex ml-14 space-x-12">
